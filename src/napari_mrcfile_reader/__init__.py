316456e8a855fd2a3de370d61df03fcd3c3a532b
alisterburt <alisterburt@gmail.com>
2023-06-08 23:19:27 +0100

    update to npe2

---
+++ src/napari_mrcfile_reader/__init__.py
@@ -0,0 +1,11 @@
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+from ._reader import napari_get_reader
+
+__all__ = (
+    "napari_get_reader",
+    )
